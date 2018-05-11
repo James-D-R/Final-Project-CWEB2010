1caf29eea2587aed4a2b56fc1958cb428bfc0d6a
remjamd <remjamd@dunwoody.edu>
2018-05-11 14:25:39 -0500

    working tables

--- src/application/AppController.java
+++ src/application/AppController.java
@@ -84,21 +84,146 @@ public class AppController implements Initializable {
     	
     	ObservableList<TableWeapons> sample = FXCollections.observableArrayList();
     	
-    	//String selectedW = WeaponBox.getValue();
-    	//System.out.println(selectedW);
+    	//create url
+    	String selectedWep = WeaponBox.getValue();
+    	selectedWep = selectedWep.toLowerCase();
+    	selectedWep = selectedWep.replace(' ', '-');
+    	String url = "https://mhworld.kiranico.com/" + selectedWep;
     	
-    	Document doc = Jsoup.connect("https://mhworld.kiranico.com/great-sword").get();  
+    	//Connect with Jsoup
+    	Document doc = Jsoup.connect(url).get();  
         Elements links = doc.select("a[href]");  
         for (Element link : links) {  
             //System.out.println("\nlink : " + link.attr("href"));
         	if (link.attr("href").contains("weapon"))
         	{
+        		String atk = "n/a";
+        		String affn = "0%";
+        		String slt = "n/a";
+        		String elm = " ";
+        		String rare = "n/a";
+        		String check;
+        		
         		String wepURL = (link.attr("href"));
         		Document wepDoc = Jsoup.connect(wepURL).get();
-        		//order: attack, affinity, slots, element, sharpness, rare
+        		//order: attack, affinity, slots, element, rare
         		Elements stats = wepDoc.getElementsByClass("lead");
-        		int total = stats.length();
-        		sample.add(new TableWeapons(link.text(),"100","200 water","+30%","0","7"));
+        		int total = stats.size();
+        		
+        		atk = stats.first().text();
+        		slt = findSlots(wepDoc); //method
+        		rare = stats.last().text();
+        		//System.out.println(total);
+        		if(selectedWep.equals("hunting-horn") || selectedWep.equals("gunlance") || selectedWep.equals("switch-axe") || selectedWep.equals("charge-blade") || selectedWep.equals("insect-glaive"))
+        		{
+        			if (total == 4) 
+        			{
+        				sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+        			}
+        			
+        			else if (total == 5) 
+        			{
+        				String checkAffn = stats.get(1).text();
+            			if(checkAffn.contains("%")) 
+            			{
+            				affn = stats.get(1).text();
+            			}
+            			elm = stats.get(2).text();
+        				sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+        			}
+        			
+        			else if (total == 6) 
+        			{
+        				String checkAffn = stats.get(1).text();
+            			if(checkAffn.contains("%")) 
+            			{
+            				affn = stats.get(1).text();
+            			}
+            			elm = stats.get(3).text();
+        				sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+        			}
+        			
+        			else if (total == 7) 
+        			{
+        				affn = stats.get(2).text();
+        				elm = stats.get(4).text();
+        				sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+        			}
+        			else 
+        			{
+        				sample.add(new TableWeapons(link.text(),"filler","filler","filler","filler","filler"));
+        			}
+        		}
+        		
+        		if (selectedWep.equals("light-bowgun") || selectedWep.equals("heavy-bowgun")) 
+        		{
+        			if (total == 5) 
+        			{
+        				sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+        			}
+        			
+        			else if (total == 6) 
+        			{
+        				String checkAffn = stats.get(1).text();
+            			if(checkAffn.contains("%")) 
+            			{
+            				affn = stats.get(1).text();
+            			}
+        				sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+        			}
+        			
+        			else if (total == 7) 
+        			{
+        				affn = stats.get(2).text();
+        				sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+        			}
+        			
+        			else 
+        			{
+        				sample.add(new TableWeapons(link.text(),"filler","filler","filler","filler","filler"));
+        			}
+        		}
+        		
+        		else 
+        		{
+        			if(total == 3) 
+            		{
+            			sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+            		}
+            		else if(total == 4) 
+            		{
+            			
+            			String checkAffn = stats.get(1).text();
+            			if(checkAffn.contains("%")) 
+            			{
+            				affn = stats.get(1).text();
+            			}
+            			elm = stats.get(2).text();
+            			sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+            		}
+            		else if(total == 5) 
+            		{
+            			String checkAffn = stats.get(1).text();
+            			if(checkAffn.contains("%")) 
+            			{
+            				affn = stats.get(1).text();
+            			}
+            			elm = stats.get(3).text();
+            			sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+            		}
+            		else if(total == 6) 
+            		{
+            			affn = stats.get(2).text();
+            			elm = stats.get(4).text();
+            			sample.add(new TableWeapons(link.text(), atk, elm, affn, slt, rare));
+            		}
+            		else 
+            		{
+            			sample.add(new TableWeapons(link.text(),"filler","filler","filler","filler","filler"));
+            		}
+        		}
+        		
+        		
         	}
             
         }  
@@ -115,7 +240,7 @@ public class AppController implements Initializable {
     		    FXCollections.observableArrayList(
     		    		"Great Sword",
     		    		"Long Sword",
-    		    		"Sword & Shield",
+    		    		"Sword",
     		    		"Dual Blades",
     		    		"Hammer",
     		    		"Hunting Horn",
@@ -131,7 +256,7 @@ public class AppController implements Initializable {
     	
 		ObservableList<String> options2 = 
     		    FXCollections.observableArrayList(
-    		    		"No Element",
+    		    		"n/a",
     		    		"Fire",
     		    		"Water",
     		    		"Thunder",
@@ -159,6 +284,42 @@ public class AppController implements Initializable {
     	
     	
 	}
+	
+	public String findSlots(Document wepDoc) 
+	{
+		Elements slots3 = wepDoc.getElementsByClass("zmdi zmdi-n-3-square");
+		Elements slots2 = wepDoc.getElementsByClass("zmdi zmdi-n-2-square");
+		Elements slots1 = wepDoc.getElementsByClass("zmdi zmdi-n-1-square");
+		Elements slotsMinus = wepDoc.getElementsByClass("zmdi zmdi-minus");
+		
+		int largeSlots = slots3.size();
+		//System.out.println(largeSlots);
+		int medSlots = slots2.size();
+		//System.out.println(medSlots);
+		int smallSlots = slots1.size();
+		//System.out.println(smallSlots);
+		int noSlots = slotsMinus.size();
+		//System.out.println(noSlots);
+		
+		int totalSlots = largeSlots + medSlots + smallSlots;
+		String slotsValue = Integer.toString(totalSlots);
+		return slotsValue;
+	}
     
+    public String checkAffinity(Document wepDoc) 
+    {
+    	String check = "false";
+    	Elements pos = wepDoc.getElementsByClass("text-success");
+    	Elements neg = wepDoc.getElementsByClass("text-danger");
+    	int a = pos.size();
+    	int b = neg.size();
+    	if (a == 1 || b == 1) 
+    	{
+    		check = "true";
+    		
+    	}
+		return check;
+    }
     
+    //public ArrayList
 }
